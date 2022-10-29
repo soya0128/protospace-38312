50ad370d6afbfd9f42a59e1be2e845e7a04983a5
soya0128 <bori.rhythem2545@gmail.com>
2022-10-29 12:11:53 +0900

    Revert "確認依頼後のリファクタリング"
    This reverts commit aed64a6aadd7648d34e9ef9fec45c88fae06869b.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,4 @@
 class PrototypesController < ApplicationController
-  before_action :set_params, only: [:show, :edit]
-  before_action :set_variable, only: [:update, :destroy]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :move_to_index, only: :edit
 
@@ -14,42 +12,38 @@ class PrototypesController < ApplicationController
 
   def create
     if Prototype.create(prototype_params)
-      redirect_to action: :index
+      redirect_to root_path
     else
       render :new
     end
   end
 
   def show
+    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
+    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    if Prototype.update(prototype_params)
-      redirect_to prototype_path(@prototype.id)
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to prototype_path(prototype.id)
     else
       render :edit
     end
   end
 
   def destroy
+    prototype = Prototype.find(params[:id])
     prototype.destroy
     redirect_to root_path
   end
 
   private
-  def set_params
-    @prototype = Prototype.find(params[:id])
-  end
-
-  def set_variable
-    prototype = Prototype.find(params[:id])
-  end
-
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
