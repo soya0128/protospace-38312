c79faa6063af763714e4fbf7cbf704e2f2aff021
soya0128 <bori.rhythem2545@gmail.com>
2022-10-29 12:19:37 +0900

    プロトタイプ詳細ページから編集ページ遷移のバグ修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :move_to_index, only: :edit
 
   def index
     @prototypes = Prototype.all
@@ -26,6 +25,10 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    @user = @prototype.user
+    unless current_user.id == @user.id
+      redirect_to root_path
+    end
   end
 
   def update
@@ -46,13 +49,6 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    @user = User.find(params[:id])
-    unless current_user.id == @user.id
-      redirect_to root_path
-    end
   end
 
 end
